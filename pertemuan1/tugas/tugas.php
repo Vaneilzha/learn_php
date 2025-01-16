28bd3074191c9dbd979c50cbf652f7ebb9f1fefa
Vaneilzha <vaneilzhakheyra@gmail.com>
2025-01-16 08:06:12 +0700

    first commit

---
+++ pertemuan1/tugas/tugas.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <?php
+            //var
+            $totalProyek = 4;
+            //$totalGaji
+            //$gajiBersih
+
+            define('pokokGaji', 5000000);
+            define('pajak', 0.12);
+            define('bonus', 500000);
+
+            $proyek *= bonus;
+            $totalGaji = gaji + $proyek;
+            $gajiBersih = $totalGaji - ($totalGaji * pajak);
+
+            echo "Bonus anda sebesar $proyek <br/>";
+            echo "Gaji bersih anda sebesar $gajiBersih <br/>";
+            echo "Total gaji anda setelah mendapatkan bonus $totalGaji <br/>";
+
+        
+        ?>    
+</body>
+</html>
