5175f86bbe5c15e5fae4935a67306474403424f2
Vaneilzha <vaneilzhakheyra@gmail.com>
2025-02-12 12:35:07 +0700

    first

---
+++ pertemuan2/tugas3.php
@@ -0,0 +1,23 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <p>Aplikasi Penghitungan Gaji</p>
+        <p>
+            Nama Lengkap : <input type="text" name="nama lengkap"> <br>
+            Divisi : <input type="text" name="divisi"> <br>
+            Total Gaji tiap bulan : <input type="number" name="total gaji tiap bulan"> <br>
+        </p>
+
+        <p>
+            <label>Apakah anda memiliki NPWP :</label>
+            <input type="checkbox" name="Iya">
+            <input type="checkbox" name="tidak">
+        </p>
+        
+</body>
+</html>
